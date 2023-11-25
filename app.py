b1b55a3826b1b0649b1a6fbd420992d668143eee
hataehyeok <105369662+hataehyeok@users.noreply.github.com>
2023-11-25 14:50:57 +0900

    feat: load collection elements in page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import glob
 
 # 세션 상태 초기화
 if 'page' not in st.session_state:
@@ -44,8 +45,35 @@ def get_collections():
 
 # 컬렉션 내용 표시 함수
 def display_collection_content(collection_name):
-    path = f"./collections/{collection_name}"
-    # 여기에 이미지, 단어, 예시 문장을 표시하는 로직 추가
+    # 각 디렉토리 경로 설정
+    image_path = f"./collections/{collection_name}/image"
+    word_path = f"./collections/{collection_name}/word"
+    sentence_path = f"./collections/{collection_name}/sentence"
+
+    # 파일 리스트 가져오기
+    images = sorted(glob.glob(f"{image_path}/*"))
+    words = sorted(glob.glob(f"{word_path}/*"))
+    sentences = sorted(glob.glob(f"{sentence_path}/*"))
+
+    # 파일이 가장 적은 디렉토리의 길이를 기준으로 함
+    min_length = min(len(images), len(words), len(sentences))
+
+    # 각 인덱스에 대해 이미지, 단어, 문장 표시
+    for i in range(min_length):
+        image, word, sentence = images[i], words[i], sentences[i]
+
+        # 이미지 표시
+        st.image(image, caption=f"Image {i}")
+
+        # 단어 표시 (텍스트 파일 읽기)
+        with open(word, 'r') as file:
+            word_content = file.read()
+        st.write(f"Word {i}: {word_content}")
+
+        # 문장 표시 (텍스트 파일 읽기)
+        with open(sentence, 'r') as file:
+            sentence_content = file.read()
+        st.write(f"Sentence {i}: {sentence_content}")
 
 # 앱 실행 로직
 if st.session_state.page == 'main':
